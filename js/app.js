6b5b6a822963fe1661c83d7b8987fe8c7dedec01
Aaron Stannard <iamstannard@gmail.com>
2015-09-15 22:11:45 +1000

    Initial commit of starter code.

---
+++ js/app.js
@@ -0,0 +1,12 @@
+/*global document, $ */
+(function () {
+    'use strict';
+
+    $(document).ready(function () {
+        /* ----- Begin writing your JavaScript here  ------*/
+        
+        
+        
+        /* ----- Finish writing your JavaScript here  -----*/
+    });
+}());
