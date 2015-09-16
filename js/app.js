ad4086a6984ec6c082448ede47c4f9703ad1442d
Aaron Stannard <iamstannard@gmail.com>
2015-09-16 21:33:02 +1000

    Add jQuery functions.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,39 @@
 /*global document, $ */
+
 (function () {
     'use strict';
 
     $(document).ready(function () {
+
         /* ----- Begin writing your JavaScript here  ------*/
-        
-        
-        
+
+        var slideDuration = 500; // milliseconds
+
+        function readMore() {
+            console.log('readMore');
+            $('#posts .readmore').hide();
+            $('#show-this-on-click').slideDown(slideDuration);
+            $('#posts .readless').show();
+        }
+
+        function readLess() {
+            console.log('readLess');
+            $('#posts .readless').hide();
+            $('#show-this-on-click').slideUp(slideDuration);
+            $('#posts .readmore').show();
+        }
+
+        function learnMore() {
+            console.log('learnMore');
+            $('#sidebar .learnmore').hide();
+            $('#learnmoretext').slideDown(slideDuration);
+        }
+
+        $('#posts .readmore').click(readMore);
+        $('.readless').click(readLess);
+        $('#sidebar .learnmore').click(learnMore);
+
         /* ----- Finish writing your JavaScript here  -----*/
+
     });
 }());
